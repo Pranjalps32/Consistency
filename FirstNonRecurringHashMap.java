5cdb41037dc14bf37d8a30bc7b3337932b27df39
Pranjalps32 <Pardeships32@gmail.com>
2026-08-24 23:18:09 +0530

    Optimized version of first non recurring character in string

---
+++ FirstNonRecurringHashMap.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class FirstNonRecurringHashMap {
+    public static void main(String[] args) {
+        HashMap<Character , Integer> hm =new HashMap<>();
+        String s="swiss";
+        for(int i=0;i<s.length();i++){
+            if(hm.containsKey(s.charAt(i))){
+                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
+            }
+            else{
+                hm.put(s.charAt(i), 1);
+            }
+        }
+        for(int i = 0; i < s.length(); i++) {
+    
+                  if(hm.get(s.charAt(i)) == 1) {
+                      System.out.println(s.charAt(i));
+                      break;
+                    }
+            }
+        
+    }
+    
+}
