6fe8eb92ec3484fee0e26a9e64608ab639a8a89c
Pranjalps32 <Pardeships32@gmail.com>
2026-08-26 17:37:14 +0530

    Day16 : Forgot yesterday

---
+++ PalindromeNum.java
@@ -0,0 +1,18 @@
+public class PalindromeNum {
+    public static void main(String[] args) {
+        int num=1221;
+        int rev=0;
+        while(num>0){
+              int d =num%10;
+              rev =rev*10+d;
+
+              num=num/10;
+        }
+        if(num==rev){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
