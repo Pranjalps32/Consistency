46555bf8097bdb262e64406c2fdd2aec1ba08a5a
Pranjalps32 <Pardeships32@gmail.com>
2026-08-27 22:27:47 +0530

    Day18 :Diamond Pattern

---
+++ Diamond.java
@@ -0,0 +1,30 @@
+public class Diamond {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<=n-1;i++){
+
+            for(int j=0;j<n-i-1;j++){
+                System.out.print("  ");
+            }
+            for(int j=0;j<i*2+1;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=0;i--){
+
+             for(int j=0;j<n-i-1;j++){
+                System.out.print("  ");
+            }
+
+            for(int j=0;j<i*2+1;j++){
+                System.out.print(" *");
+            }
+
+           
+            
+            System.out.println();
+        }
+    }
+    
+}
