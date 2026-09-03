ae7c568e05ef0cedbeabe21aa05c2503b9d7e6b6
Pranjalps32 <Pardeships32@gmail.com>
2026-09-03 17:17:51 +0530

    Day24 : Fibonacci series

---
+++ Fibonacciseries.java
@@ -0,0 +1,13 @@
+public class Fibonacciseries {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=10;
+        for(int i=1;i<n;i++){
+            System.out.print(a + " " );
+            int c =a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
